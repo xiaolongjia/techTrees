9fbe848c495cb4c761771f0efe692989f481e691
jiaxiaolong <jiaxiaolong>
2020-01-20 23:02:06 -0600

    postgreSQL

---
+++ DB_PostgreSQL/test.py
@@ -0,0 +1,11 @@
+#!C:\Python\python
+
+
+import psycopg2
+conn = psycopg2.connect(database='mydb',user='postgres',password='1234',host='127.0.0.1',port='5432')
+cur = conn.cursor()
+cur.execute("SELECT * FROM cities")
+rows = cur.fetchall()
+print(rows)
+conn.commit()
+cur.close()
