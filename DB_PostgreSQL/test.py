ae2e961a2f482d94fa411a94f99d6fa08f785998
jiaxiaolong <jiaxiaolong>
2020-10-18 23:38:53 -0600

    sql,postgresql updates

--- DB_PostgreSQL/test.py
+++ DB_PostgreSQL/test.py
@@ -1,8 +1,8 @@
 #!C:\Python\python
 
-
 import psycopg2
-conn = psycopg2.connect(database='mydb',user='postgres',password='1234',host='127.0.0.1',port='5432')
+
+conn = psycopg2.connect(database='mydb',user='postgres',password='121119',host='127.0.0.1',port='5432')
 cur = conn.cursor()
 cur.execute("SELECT * FROM cities")
 rows = cur.fetchall()
